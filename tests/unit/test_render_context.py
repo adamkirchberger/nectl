05fb6c041145febee30b685e12e775081f994854
Adam Kirchberger <adamkirchberger@users.noreply.github.com>
2022-01-11 11:15:13 +0000

    refactor: rename render facts method

--- tests/unit/test_render_context.py
+++ tests/unit/test_render_context.py
@@ -16,14 +16,14 @@
 # along with Nectl.  If not, see <http://www.gnu.org/licenses/>.
 
 from nectl.configs.render import _render_context
-from nectl.configs.template_utils import get_host_facts
+from nectl.configs.template_utils import get_render_facts
 
 
 def test_should_return_empty_dict_when_returning_host_facts_with_default_context():
     # GIVEN that we are not running in a template and render context is blank
 
     # WHEN getting facts
-    facts = get_host_facts()
+    facts = get_render_facts()
 
     # THEN expect result to be empty dict
     assert facts == {}
@@ -40,7 +40,7 @@ def test_should_return_empty_dict_when_returning_host_facts_with_context_set():
     _render_context.set({"facts": mock_facts})
 
     # WHEN getting facts
-    facts = get_host_facts()
+    facts = get_render_facts()
 
     # THEN expect result to match mock facts
     assert facts == mock_facts
@@ -48,7 +48,7 @@ def test_should_return_empty_dict_when_returning_host_facts_with_context_set():
     # GIVEN that we are not running in a template and render context is blank
 
     # WHEN getting facts
-    facts = get_host_facts()
+    facts = get_render_facts()
 
     # THEN expect result to be empty dict
     assert facts == {}
@@ -65,7 +65,7 @@ def test_should_return_empty_dict_when_returning_host_facts_with_context_set():
     _render_context.set({"facts": mock_facts})
 
     # WHEN getting facts
-    facts = get_host_facts()
+    facts = get_render_facts()
 
     # THEN expect result to match mock facts
     assert facts == mock_facts
