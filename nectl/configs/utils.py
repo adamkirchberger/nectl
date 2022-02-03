eb31d90bdc1343e35b4edc8d915c113dc2527032
Adam Kirchberger <adamkirchberger@users.noreply.github.com>
2022-02-03 09:13:39 +0000

    fix: update writer to skip blank configs and return total

--- nectl/configs/utils.py
+++ nectl/configs/utils.py
@@ -28,7 +28,7 @@ logger = get_logger()
 
 def write_configs_to_dir(
     configs: Dict[str, str], output_dir: str, extension: str = "txt", replace=True
-) -> None:
+) -> int:
     """
     Writes supplied configs dict to an output directory using the key as the
     filename and value as content.
@@ -38,9 +38,13 @@ def write_configs_to_dir(
         output_dir (str): directory to write files to.
         extension (str): optional file extension to use. Defaults to "txt".
         replace (bool): Delete any existing files when writing. Defaults to True.
+
+    Returns:
+        int: total created files.
     """
+    total = 0
     ts_start = time.perf_counter()
-    logger.debug("starting writing config files")
+    logger.debug("start writing config files")
 
     # Wipe output dir
     if replace:
@@ -48,18 +52,22 @@ def write_configs_to_dir(
             shutil.rmtree(output_dir)
             logger.debug(f"deleted output dir: {output_dir}")
         except FileNotFoundError:
-            logger.debug("skipped delete output dir not found: {output_dir}")
+            logger.debug(f"skipped delete output dir not found: {output_dir}")
 
     # Ensure output directory exists
     os.makedirs(output_dir, exist_ok=True)
 
     # Loop through each host config
     for host, conf in configs.items():
-        filename = f"{output_dir}/{host}.{extension}"
-        with open(filename, "w") as fh:
-            fh.write(conf)  # write config
-            fh.write("\n")  # write config with newline at EOF
-            logger.debug(f"config written to file: {filename}")
+        if conf:
+            filename = f"{output_dir}/{host}.{extension}"
+            with open(filename, "w") as fh:
+                fh.write(conf)  # write config
+                fh.write("\n")  # write config with newline at EOF
+                logger.debug(f"config written to file: {filename}")
+            total += 1
 
     dur = f"{time.perf_counter()-ts_start:0.4f}"
     logger.info(f"finished writing config files ({dur}s)")
+
+    return total
