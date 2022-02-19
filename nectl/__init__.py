1c1bceebba767f92bd35b23691a8cbea7a9bd46c
Adam Kirchberger <adamkirchberger@users.noreply.github.com>
2022-02-19 19:35:37 +0000

    feat: update public methods

--- nectl/__init__.py
+++ nectl/__init__.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Nectl.  If not, see <http://www.gnu.org/licenses/>.
 
-from .data.actions import Actions
+from .data.actions import Actions as actions
+from .configs.template_utils import get_render_facts
 
-actions = Actions
+
+__all__ = ["actions", "get_render_facts"]
